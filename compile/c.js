be317d6be9ef1cfaad3ee8bcea685cbe5948b3af
busbtvi <busbtvi@gmail.com>
2020-11-12 20:40:52 +0900

    fix bug

--- compile/c.js
+++ compile/c.js
@@ -1,34 +1,18 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const fs = require('fs');
+const fs = require('fs').promises;
 
-async function compile_run(inFile){
+// do it(error check);
+exports.check = async (code,inFile,outFile) => {
+  var writefile = await fs.writeFile('a.c',code);
   var compile = await exec("gcc a.c -o a.exe");
   var run = await exec(`a.exe < ${inFile}`);
-
-  // if(compile.stderr || run.stderr){
-  //   console.log(compile.stderr);
-  //   console.log(run.stderr);
-
-  //   return {
-  //     compileError: compile.stderr,
-  //     runtimeError: run.stderr
-  //   };
-  // }
-  return run.stdout;
-}
-function compare(stdout,outFile){
-  var ans = fs.readFileSync(outFile,'utf-8');
-
-  return (stdout == ans);
-}
-
-exports.check = async (inFile,outFile) => {
-  var runResult = await compile_run(inFile);
-  var checkResult = compare(runResult,outFile);
-
+  var readFile = await fs.readFile(outFile,'utf-8');
+  
+  await fs.unlink('a.c');
+  await fs.unlink('a.exe');
   return {
-    result : runResult,
-    state : checkResult
+    result : run.stdout,
+    state : (run.stdout == readFile)
   };
 }
--- index.js
+++ index.js
@@ -4,37 +4,24 @@ const app = express();
 const fs = require('fs');
 
 app.use(bodyParser.json());
-// app.use(express.bodyParser());
 // app.use("/pnu/cse",cse);
 
-const LangExtensionMap = {
-  c : "c",
-  cpp : "cpp",
-  python : "py",
-  javascript : "js",
-};
-
-app.post("/exec",(req,res) => {
+app.post("/exec",async (req,res) => {
   var problemNum = req.body.problemNum;
   var caseNum = req.body.caseNum;
   var lang = req.body.lang;
   var code = req.body.code;
 
-  fs.writeFile(`a.${ LangExtensionMap[lang] }`,code,async (err) => {
-    if (err) return console.log(err);
-
-    var inFile = `./problem/${problemNum}/${caseNum}.in`;
-    var outFile = `./problem/${problemNum}/${caseNum}.out`;
-    
-    const compileRunCheck = require(`./${lang}.js`);
-    var result = await compileRunCheck.check(inFile,outFile);
-
-    res.send(result);
-  });
+  const compileRunCheck = require(`./compile/${lang}.js`);
+  var result = await compileRunCheck.check(
+    code,
+    `./compile/problem/${problemNum}/${caseNum}.in`,
+    `./compile/problem/${problemNum}/${caseNum}.out`
+  );
+  res.send(result);
 });
-app.get('/',(req,res) => {
-  res.sendFile('C:/Users/Semicolons20602/Desktop/personal test/CCM_clean/editor/test.html');
-});
+
+app.get('/',(req,res) => res.sendFile(__dirname + '/editor/test.html'));
 
 app.listen(4000,() => {
   console.log("server is running");
