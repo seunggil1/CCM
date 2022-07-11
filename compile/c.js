580652d64e486828378eff93ee8711bb9adab9e5
seung <ksgg1navercom@gmail.com>
2022-07-11 23:59:39 +0900

    Update c.js

--- compile/c.js
+++ compile/c.js
@@ -6,15 +6,16 @@ const fs = require('fs').promises;
 exports.check = async (code,inFile,outFile) => {
   var readFile = await fs.readFile(outFile,'utf-8');
   var writefile = await fs.writeFile('a.c',code);
-  var compile = await exec("gcc a.c -o a.exe");
+  // var compile = await exec("gcc a.c -o a.exe");
+  var compile = await exec("gcc a.c");
 
   var pre_time = Date.now();
-  var run = await exec(`a.exe < ${inFile}`);
+  var run = await exec(`a.out < ${inFile}`);
   var cur_time = Date.now();
   
   
   await fs.unlink('a.c');
-  await fs.unlink('a.exe');
+  await fs.unlink('a.out');
   return {
     time : cur_time - pre_time,
     output : run.stdout,
